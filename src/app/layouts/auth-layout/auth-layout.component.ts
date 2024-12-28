7e60e44f41b8d2e8b406eabf0677a51a23d6314c
sasindu <sasindudil0002@gmail.com>
2024-12-28 12:07:41 +0530

    added fadeInOut animation to auth layout

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { trigger, animate, transition, style } from '@angular/animations';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [],
   templateUrl: './auth-layout.component.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-in-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('1s ease-in-out', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class AuthLayoutComponent {
   @Input() imageSrc: string = ''; // Input for image source
